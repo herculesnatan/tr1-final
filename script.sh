e097ac61dabbc7904c4d6a99810bd33b2c0a5832
Hércules Natan <medeiroshercules23@gmail.com>
2025-06-22 21:51:44 -0300

    script.sh rodando bem

--- script.sh
+++ script.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# Abrir um único terminal com múltiplas abas (se usar GNOME Terminal)
-gnome-terminal --tab --title="Servidor" -- bash -c "python3 main_server.py; exec bash" \
-
+konsole --new-tab -e bash -c "python3 main_server.py; exec bash" &
 sleep 1
-gnome-terminal --tab --title="Cliente 1" -- bash -c "python3 main_client.py; exec bash" \
+konsole --new-tab -e bash -c "python3 main_client.py; exec bash" &
 
-gnome-terminal --tab --title="Cliente 2" -- bash -c "python3 main_client.py; exec bash"
-
-# Mensagem de conclusão
 echo "Todos os processos foram iniciados em abas separadas."
+
+
+"""
+usar esse comandos para conseguir compilar os códigos
+sudo apt install konsole
+chmod +x script.sh
+./script.sh
+
+"""
