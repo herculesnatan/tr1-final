4463cae61576a1ac5fcc56cd7ebe9ca9088a0ebe
Hércules Natan <medeiroshercules23@gmail.com>
2025-07-02 15:07:16 -0300

    ajustes no ruido

--- camada_fisica/ruido.py
+++ camada_fisica/ruido.py
@@ -1,13 +1,13 @@
-import random as rand
+import random as r
 
-taxa_de_erro = 0.0001
+err_tax = 0.03
 def flip_random_bit(binary_list):
     # Faz uma cópia da lista original
     binary_list_copy = list(binary_list)
     
     # Percorre cada bit individualmente
     for i in range(len(binary_list_copy)):
-        if rand.random() < taxa_de_erro:  # Probabilidade de alteração para cada bit
+        if r.random() < err_tax:  # Probabilidade de alteração para cada bit
             binary_list_copy[i] = '1' if binary_list_copy[i] == '0' else '0'
     
     # Retorna a cópia alterada
